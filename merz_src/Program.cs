f32c7a4c681fb5b919b1a5801c1e6b8e3015cb79
Leonid Chebenko <lioncbehebnko@gmail.com>
2025-06-11 09:10:00 +1000

    Updates version check and removes unused code
    Refines the version checking mechanism to improve clarity and user experience.
    Removes redundant output statements and an unnecessary project reference, streamlining the codebase.

--- merz_src/Program.cs
+++ merz_src/Program.cs
@@ -203,19 +203,14 @@ namespace merz
                     var form = OptionsForm.Create("Merz " + "V: " + version);
 
                     string latestGitHubTag = GetLatestGitHubVersionAsync().GetAwaiter().GetResult();
-                    Out.WriteLine("Latest GitHub version: " + latestGitHubTag);
                     if (!string.IsNullOrEmpty(latestGitHubTag))
                     {
                         string latestVersionStr = latestGitHubTag.TrimStart('v'); // "1.0.1"
-                                                                                  //print the latest version and current version
-                        Out.WriteLine("Current version: " + version);
-                        Out.WriteLine("Latest version from GitHub: " + latestVersionStr);
                         if (Version.TryParse(version, out Version currentV) &&
                             Version.TryParse(latestVersionStr, out Version latestV))
                         {
                             if (latestV > currentV)
                             {
-                                Out.WriteLine("New version available within loop: " + latestVersionStr);
                                 form.Options.AddTextLabel($"New version available: {latestVersionStr}");
                                 string downloadLinkHtml = $"Download from GitHub: <a href=\"https://github.com/precision-mining-consulting/merz/releases/latest\">https://github.com/precision-mining-consulting/merz/releases/latest</a>";
 
@@ -278,7 +273,6 @@ namespace merz
 
         }
 
-        //static checkNewWebVersion()
         private static async Task<string> GetLatestGitHubVersionAsync()
         {
             using (var httpClient = new HttpClient())
